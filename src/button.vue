f8589b4b618ecf455072fdcbadd0f3c0e86d4c84
pergeryfly <pergeryfly@gmail.com>
2020-05-26 15:47:15 +0800

    添加icon，并使用iconPositon定位icon的位置，同时验证iconPosition的值为left和right中的一个

--- src/button.vue
+++ src/button.vue
@@ -1,27 +1,77 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button :class="{[`icon-${iconPosition}`]:true}" class="g-button">
+        <svg aria-hidden="true" v-if="icon">
+            <use :xlink:href=`#i-${icon}`></use>
+        </svg>
+        <span class="text"><slot></slot></span>
+    </button>
 </template>
 
 <script>
     export default {
+        // props:['icon','iconPosition']
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator: function (value) {
+                    console.log(value)
+                    if (value != 'left' && value != 'right') {
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+            }
+        }
     }
+
 </script>
 
 <style lang="scss">
-    .g-button{
-        height:var(--button-height);
+    .g-button {
+        height: var(--button-height);
         padding: 0 1em;
-        border-radius:var(--border-radius);
-        border:1px solid var(--border-color);
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
         background: var(--button-bg);
-        &:hover{
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: top;
+
+        &:hover {
             border-color: var(--border-color-hover);
         }
-        &:active{
+
+        &:active {
             background-color: var(--button-active-bg);
         }
-        &:focus{
+
+        &:focus {
             outline: none;
+        }
+
+        > svg {
+            order: 1;
+            margin-right: .3em;
+        }
+
+        > .text {
+            order: 2;
+        }
+
+        &.icon-right {
+            > svg {
+                order: 2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
+
+            > .text {
+                order: 1;
+            }
         }
     }
 
